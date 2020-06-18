73e346ba659805a59833669da53960bd3beeb6bc
federico bolzan <federicobolzan4@gmail.com>
2020-06-18 15:43:37 +0200

    ale

---
+++ SiloApplication/SiloApplication.Models/Lambda.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SiloApplication.Models
+{
+    public class Lambda
+    {
+        public int Id { get; set; }
+        public decimal? LiquidLevel { get; set; }
+        public decimal? Pressure { get; set; }
+        public int Temperature { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
