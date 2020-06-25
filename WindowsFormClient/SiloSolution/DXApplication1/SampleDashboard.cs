b47f3b4ed3f0457a804119216f8fdb8d88c58bbf
federico bolzan <federicobolzan4@gmail.com>
2020-06-25 14:27:35 +0200

    final
    final

---
+++ WindowsFormClient/SiloSolution/DXApplication1/SampleDashboard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.DataAccess;
+using DevExpress.DashboardCommon;
+
+namespace DXApplication1
+{
+    public partial class SampleDashboard : DevExpress.DashboardCommon.Dashboard
+    {
+        public SampleDashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
